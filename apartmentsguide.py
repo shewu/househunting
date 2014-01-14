621e166dc5c0c69b8002541672e13b9bd4542802
Sherry Wu <shewu19@gmail.com>
2014-01-14 01:43:10 -0500

    added api for apartmentsguide.com

--- apartmentsguide.py
+++ apartmentsguide.py
@@ -4,21 +4,23 @@ from bs4 import BeautifulSoup
 import urllib
 
 
-URL_BASE = 'http://www.apartments.com'
+URL_BASE = 'http://www.apartmentsguide.com'
 
 
-def search_city(city):
-	f = urllib.urlopen('%(base)s/search/?query=%(city)s' % {
+def search_city(city, state='California'):
+	f = urllib.urlopen('%(base)s/apartments/%(state)s/%(city)s' % {
 		'base': URL_BASE,
-		'city': city.replace(' ', '%%20')
+		'state': state,
+		'city': city.replace(' ', '-')
 	})
 	bs = BeautifulSoup(f.read())
-	search_results = bs.find_all(itemprop='url', href=True)
+	search_results = bs.find_all(attrs={'class': 'srp_title', 'href': True})
 
 	apartments = []
 
 	for search_result in search_results:
 		follow_link = search_result['href']
+		print follow_link
 		f2 = urllib.urlopen(URL_BASE + follow_link)
 		bs2 = BeautifulSoup(f2.read())
 
--- househunting.py
+++ househunting.py
@@ -1,22 +1,58 @@
 import apartmentscom
+import apartmentsguide
 
 import googlemaps
 
+apple = '1 infinite loop 95014'
+
+
+def meters_to_miles(meters):
+	return int(meters) / 1600
+
+
+def get_directions(start, end, gmaps):
+	directions = gmaps.directions(start, end)
+	print "=== %s > %s ===" % (start, end,)
+	print meters_to_miles(directions['Directions']['Distance']['meters'])
+	print directions['Directions']['Duration']['html']
+
+
+def to_caltrain_station(address, city, gmaps):
+	start = address
+	end = city + ' caltrain station'
+	get_directions(start, end, gmaps)
+
+
+def to_apple(address, gmaps):
+	get_directions(address, '1 infinite loop 95014', gmaps)
+
+
+def to_derpbox(address, gmaps):
+	get_directions(address, '185 berry st san francisco', gmaps)
+
+
+def redwood_city(gmaps):
+	apartments = set(
+		apartmentscom.search_city('redwood city')
+		+ apartmentsguide.search_city('redwood city')
+	)
+	caltrain_stop = 'redwood city caltrain station'
+
+	for apartment in apartments:
+		to_caltrain_station(apartment, 'redwood city', gmaps)
+		to_apple(apartment, gmaps)
+
 
 def san_mateo(gmaps):
-	apartments = [
-		'203 laurie meadows drive 94403',
-		'55 west 5th avenue 94402',
-		'3110 casa de campo 94403',
-		'888 n. san mateo dr. 94401',
-	]
+	apartments = set(
+		apartmentscom.search_city('san mateo')
+		+ apartmentsguide.search_city('san mateo')
+	)
 	caltrain_stop = 'san mateo caltrain station'
 
 	for apartment in apartments:
-		directions = gmaps.directions(apartment, caltrain_stop)
-		print "===", apartment, "==="
-		print directions['Directions']['Distance']
-		print directions['Directions']['Duration']['html']
+		to_caltrain_station(apartment, 'san mateo', gmaps)
+		to_apple(apartment, gmaps)
 
 
 def googlemaps_example():
@@ -31,7 +67,8 @@ def googlemaps_example():
 def main():
 	#googlemaps_example()
 	gmaps = googlemaps.GoogleMaps('AIzaSyBHMOZfCCSElPKRh7ub4FAiXWnuGarO5Mo')
-	print apartmentscom.search_city('san mateo')
+	san_mateo(gmaps)
+	#redwood_city(gmaps)
 
 if __name__ == '__main__':
 	main()
