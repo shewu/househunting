1535e21c2371728fd7fa2924072ce5db127a50b1
Sherry Wu <shewu19@gmail.com>
2014-02-21 03:24:56 -0500

    added more cities and cleaned up abstractions

--- househunting.py
+++ househunting.py
@@ -3,8 +3,9 @@ import apartmentsguide
 
 import googlemaps
 
+
 class Result(object):
-	def __init__(self, dist, duration):
+	def __init__(self, dist=float('inf'), duration=float('inf')):
 		self._dist = dist
 		self._duration = duration
 	
@@ -25,6 +26,9 @@ class MetricsResults(object):
 		self._apartment = apartment
 		self._result_caltrain = result_caltrain
 		self._result_apple = result_apple
+
+	def eval(self):
+		return self.result_caltrain.duration**2 * self.result_apple.duration
 
 	@property
 	def apartment(self):
@@ -53,11 +57,15 @@ def seconds_to_minutes(seconds):
 
 
 def calculate_directions(start, end, gmaps):
-	directions = gmaps.directions(start, end)
-	return Result(
-		dist=meters_to_miles(directions['Directions']['Distance']['meters']),
-		duration=seconds_to_minutes(directions['Directions']['Duration']['seconds'])
-	)
+	try:
+		directions = gmaps.directions(start, end, mode='transit')
+		return Result(
+			dist=meters_to_miles(directions['Directions']['Distance']['meters']),
+			duration=seconds_to_minutes(directions['Directions']['Duration']['seconds'])
+		)
+	except Exception, e:
+		print "Exception:", e
+		return Result()
 
 
 def to_caltrain_station(address, city, gmaps):
@@ -83,33 +91,48 @@ def apts_in(city, gmaps):
 
 	metrics = []
 
-	for apartment in apartments:
-		metrics.append(
-			MetricsResults(
-				apartment=apartment,
-				result_caltrain=to_caltrain_station(apartment, city, gmaps),
-				result_apple=to_apple(apartment, gmaps)
-			)
+	metrics = [
+		MetricsResults(
+			apartment=apartment,
+			result_caltrain=to_caltrain_station(apartment, city, gmaps),
+			result_apple=to_apple(apartment, gmaps)
 		)
+		for apartment in apartments
+	]
 
 	return metrics
 
 
-def googlemaps_example():
+def googlemaps_example(gmaps):
+	"""
+	Simple demonstration and reference of googlemaps package.
+	"""
 	# documentation: http://py-googlemaps.sourceforge.net/
 	start = '3 ames street 02142'
 	end = '465 bolivar st 02021'
 
-	directions = gmaps.directions(start, end)
-	print directions['Directions']['Duration']
+	# testing kwargs
+	directions = gmaps.directions(start, end, kwargs={'mode': 'transit', 'avoid': 'highways', 'units': 'imperial'})
+	route = directions['Directions']['Routes'][0]
+	for step in route['Steps']:
+		print step['descriptionHtml']
 
 
 def main():
 	gmaps = googlemaps.GoogleMaps('AIzaSyBHMOZfCCSElPKRh7ub4FAiXWnuGarO5Mo')
 	all_results = apts_in('san mateo', gmaps) \
-		+ apts_in('redwood city', gmaps)
+		+ apts_in('redwood city', gmaps) \
+		+ apts_in('burlingame', gmaps) \
+		+ apts_in('south san mateo', gmaps) \
+		+ apts_in('redwood shores', gmaps) \
+		+ apts_in('belmont', gmaps) \
+		+ apts_in('foster city', gmaps) \
+		+ apts_in('san carlos', gmaps) \
+		+ apts_in('millbrae', gmaps) \
+		+ apts_in('san bruno', gmaps) \
+		+ apts_in('hillsborough', gmaps)
 
-	for result in sorted(all_results, key=lambda mr: mr.result_caltrain.duration + mr.result_apple.duration):
+	for result in sorted(all_results, key=lambda mr: mr.result_caltrain.duration * mr.result_apple.duration):
 		print result
 
 if __name__ == '__main__':
